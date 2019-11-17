b3ea58238c37f853842a452d023a373f0bd16b21
Michael Ploughman <michael@michaelhploughman.com>
2019-11-17 16:36:26 -0700

    map working, resultsView ready for testing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const STORE = {
 
 // URLs
 const YELP_API_URL = 'https://api.yelp.com/v3/businesses/search';
-const MAPBOX_API_URL ='';
+const MAPBOX_API_URL = '';
 // KEYS
 const YELP_API_KEY = 'ZfWIRiwsSHvSYOQ3gxUAB6mY8RyQ1zKwv30vI274WbruCXISg8n5TN-NBFpVfqnDsM4_AWjmONrljsNLzTPuITMswrBWOwmL9H0NNrw71qpxPWfm2kFfniP3s23QXXYx';
 const MAPBOX_API_KEY = 'pk.eyJ1IjoibWljaGFlbGhwIiwiYSI6ImNrMzF1NjkyODBkMGwzbXBwOWJrcXQxOWwifQ.5VGC7vYD6ckQ2v-MVsIHLw';
@@ -27,35 +27,46 @@ const mapboxOptions = {
 };
 
 // GET DATA
-function getBarsFromYelp() {
+function getBars() {
   fetch()
-  .then(res => res.json())
-  .then(resJson => 
-    renderResults(resJson))
-  .catch(e => console.log(e));
+    .then(res => res.json())
+    .then(resJson =>
+      console.log(resJson))
+    .catch(e => console.log(e));
 }
 
 function getMapData() {
   fetch()
-  .then(res => res.json())
-  .then(resJson => 
-    renderResults(resJson))
-  .catch(e => console.log(e));
+    .then(res => res.json())
+    .then(resJson =>
+      renderResults(resJson))
+    .catch(e => console.log(e));
+}
+
+function buildMap(startBar) {
+  mapboxgl.accessToken = MAPBOX_API_KEY;
+  let map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v9',
+  center: startBar,
+  zoom: 13,
+});
 }
 
 function getDirections() {
   fetch()
-  .then(res => res.json())
-  .then(resJson => 
-    renderResults(resJson))
-  .catch(e => console.log(e));
+    .then(res => res.json())
+    .then(resJson =>
+      renderResults(resJson))
+    .catch(e => console.log(e));
 }
 
 // EVENT LISTENERS
 function watchForm() {
   $('form').submit(e => {
     e.preventDefault();
-
+    getBarsFromYelp();
+    getMapData();
   })
 }
 
@@ -65,36 +76,59 @@ function watchModifiers() {
   })
 }
 
+function watchADVSearch() {
+  $('.searchForm').on('click', '#advSearchToggle', function(e) {
+    e.preventDefault();
+    $('.advSearchOptions').slideToggle('slow');
+  });
+}
+
 // VIEW HANDLERS
-function determineView(state) {
-  if(state === 'MAIN') {
+function determineView(state, res) {
+  if (state === 'MAIN') {
     return buildMainView();
   } else if (state === 'RESULTS') {
-    return buildResultsView();
+    return buildResultsView(res);
   } else if (state === 'BAD RESULT') {
-    return buildBadResults();
+    return buildBadResults(res);
   }
 }
 
 function buildMainView() {
-  $('results').html('');
-  const view = ``;
-  return view;
+  $('.results').html('');
+  $('.map').html('');
 }
 
 function buildResultsView(res) {
-  $('results').html('');
-  let view = ``;
-  return view;
+  const bars = res;
+  $('.results').html('');
+  $('.map').html('');
+  let resultView = [];
+  for(let i = 0; i < bars.length; i++) {
+    resultView.push(`<div class="barCard">
+      <h3 class="barTitle barLink">
+        <a href="${bars[i].website_url}">${bars[i].name}</a>
+      </h3>
+      <p>${bars[i].street}</p>
+      <p>${bars[i].city}, ${bars[i].city}, ${bars[i].state}, ${bars[i].postal_code}</p>
+      <p class="barPhone">${bars[i].phone}</p>
+    `);
+  }
+  resultView.join('');
+  $('.results').html(resultView);
+  $('.map').html(buildMap());
 }
 
 function buildBadResults(res) {
-  $('results').html('');
-  let view = ``;
-  return view;
+  $('.results').html('');
+  $('.map').html('');
+  let view = `<h2>We've experienced an error</h2>
+  <p>${res.message}</p>`;
+  $('.results').html(view);
 }
 
 // PAGE READY LISTENER
 $(function() {
   watchForm();
+  watchADVSearch();
 });
