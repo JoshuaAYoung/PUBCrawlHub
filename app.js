ac1e33f68c844cd907a19bc91a6b6ffbf15fadbe
Josh Young <joshua@young.net>
2019-11-17 21:16:30 -0700

    open brewery fully functional

--- app.js
+++ app.js
@@ -2,13 +2,65 @@
 // STORE
 const STORE = {
   state: 'MAIN',
-  stateNumber: 0
+  stateNumber: 0,
+  brewList: [],
+  stateCodes: {
+    AK: "Alaska",
+    AL: "Alabama",
+    AR: "Arkansas",
+    AZ:	"Arizona",
+    CA:	"California",
+    CO:	"Colorado",
+    CT:	"Connecticut",
+    DC: "Washington DC",
+    DE:	"Delaware",
+    FL:	"Florida",
+    GA:	"Georgia",
+    GU:	"Guam",
+    HI:	"Hawaii",
+    IA:	"Iowa",
+    ID:	"Idaho",
+    IL:	"Illinois",
+    IN:	"Indiana",
+    KS:	"Kansas",
+    KY:	"Kentucky",
+    LA:	"Louisiana",
+    MA:	"Massachusetts",
+    MD:	"Maryland",
+    ME:	"Maine",
+    MI:	"Michigan",
+    MN:	"Minnesota",
+    MO:	"Missouri",
+    MS:	"Mississippi",
+    MT:	"Montana",
+    NC:	"North Carolina",
+    ND:	"North Dakota",
+    NE:	"Nebraska",
+    NH:	"New Hampshire",
+    NJ:	"New Jersey",
+    NM:	"New Mexico",
+    NV:	"Nevada",
+    NY:	"New York",
+    OH:	"Ohio",
+    OK:	"Oklahoma",
+    OR:	"Oregon",
+    PA:	"Pennsylvania",
+    PR:	"Puerto Rico",
+    RI:	"Rhode Island",
+    SC:	"South Carolina",
+    SD:	"South Dakota",
+    TN:	"Tennessee",
+    TX:	"Texas",
+    UT:	"Utah",
+    VA:	"Virginia",
+    VI:	"Virgin Islands",
+    VT:	"Vermont",
+    WA:	"Washington",
+    WI:	"Wisconsin",
+    WV:	"West Virginia",
+    WY:	"Wyoming"
+  }
 }
-
-
-
-
-
 
 // URLs
 const MAPBOX_URL = 'https://api.mapbox.com/';
@@ -31,39 +83,15 @@ const mapboxOptions = {
   )
 };
 
-// BEERMAPPING
-
-// function formatBMQuery(parameters) {
-
-//   //returns the array object as a single string with & in between each
-//     return queryItems.join('&');
-// }
-
-// function getBarsFromBM(locationQ, radiusQ, limitQ, priceQ, openQ, termQ) {
-//   const bMURL = 'http://beermapping.com/webservice/loccity/';
-//   const bMKey = "658bca16f97e42e28a59c3bdb54241f8";
-  
-//   //sets queryString variable as the full string of every parameter joined together
-//   const url = bMURL + bMKey + '/' + STORE[1].city + ',' + STORE[1].state; + "&s=json"
-
-//   console.log(url);
-
-//   fetch(url)
-//   .then(response => {
-//     if (response.ok) {
-//       STATE[0].stateNumber++;
-//       return response.json();
-//     }
-//     throw new Error(response.statusText)
-//   })
-//   // .then(responseJson => determineView(STORE.state, responseJson))
-//   .then(responseJson => console.log(responseJson))
-//   .catch(err => {
-//     determineView(STORE.state, err)
-//   })
-// }
-
 // OPEN BREWERY
+function convertAbbrev(input) {
+  if (input.length === 2) {
+    return STORE.stateCodes[input.toUpperCase()];
+  }
+  else {
+    return input;
+  }
+}
 
 function formatQuery(parameters) {
   //takes parameter keys and makes an array out of them
@@ -97,39 +125,45 @@ function getBarsFromOB(cityQ, stateQ, limitQ=10) {
     }
     throw new Error(response.statusText)
   })
-  .then(responseJson => console.log(responseJson))
-  // .then(responseJson => determineView(STORE.state, responseJson))
+  .then(responseJson => { 
+    determineView(STORE.state, responseJson);
+    STORE.brewList = responseJson})
   .catch(err => {
     STORE.state = "BAD RESULTS";
     determineView(STORE.state, err)
   })
 }
 
-function buildMap(startBar) {
-  mapboxgl.accessToken = MAPBOX_API_KEY;
-  let map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v9',
-  center: startBar,
-  zoom: 13,
-});
-}
+// function buildMap(startBar) {
+//   mapboxgl.accessToken = MAPBOX_API_KEY;
+//   let map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v9',
+//   center: startBar,
+//   zoom: 13,
+// });
+// }
 
-function getDirections() {
-  const url = `https://api.mapbox.com/directions/v5/mapbox/walking/-73.989%2C40.733%3B-74%2C40.733.json?access_token=pk.eyJ1IjoibWljaGFlbGhwIiwiYSI6ImNrMzF1NjkyODBkMGwzbXBwOWJrcXQxOWwifQ.5VGC7vYD6ckQ2v-MVsIHLw`
-  fetch(url)
-    .then(res => res.json())
-    .then(resJson =>
-      console.log(resJson))
-    .catch(e => console.log(e));
-}
+// function getDirections() {
+//   const url = `https://api.mapbox.com/directions/v5/mapbox/walking/-73.989%2C40.733%3B-74%2C40.733.json?access_token=pk.eyJ1IjoibWljaGFlbGhwIiwiYSI6ImNrMzF1NjkyODBkMGwzbXBwOWJrcXQxOWwifQ.5VGC7vYD6ckQ2v-MVsIHLw`
+//   fetch(url)
+//     .then(res => res.json())
+//     .then(resJson =>
+//       console.log(resJson))
+//     .catch(e => console.log(e));
+// }
 
 // EVENT LISTENERS
 function watchForm() {
-  $('form').submit(e => {
+  $('.searchForm').on('submit', function(e){
     e.preventDefault();
-    getBarsFromYelp();
-    getMapData();
+    let cityInput = $(this).find('input[name="mainSearch"]').val();
+    let stateInput = convertAbbrev($(this).find('input[name="stateSearch"]').val());
+    let zipcodeInput = $(this).find('input[name="zipSearch"]').val();
+    let limitInput = $(this).find('input[name="resultsNumber"]').val();
+    let radiusInput = $(this).find('input[name="proximitySearch"]').val();
+    getBarsFromOB(cityInput, stateInput, limitInput);
+    // getMapData();
   })
 }
 
